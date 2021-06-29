7fb105c50ac56b3276bffe98cff901447c4dbd9c
kazuki.yanagisawa <syoka467@gmail.com>
2021-06-29 14:26:26 +0000

    create webapp

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '1635f886d7a2a83388c68b8a1f96aac24b68f4a610b59edc00d04480e62c04c10b8209a4f33cc61c0af372f3d256dcd84101da3d6660e1b15a1ac60877435cdb'
